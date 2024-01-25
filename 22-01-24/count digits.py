4f2d1e7e289d0e6d62f7b0e4f2d18642649d8b4b
SachinDevB <151764882+SachinDevB@users.noreply.github.com>
2024-01-25 20:00:54 +0530

    Create count digits.py

---
+++ 22-01-24/count digits.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    # Input the number
+    N = int(input().strip())
+
+    # Count the number of digits
+    num_digits = len(str(N))
+
+    # Output the result
+    print(num_digits)
