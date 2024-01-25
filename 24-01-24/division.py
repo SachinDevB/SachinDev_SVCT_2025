aecc64b282902f67b3648660dbbbf4fb6d74175b
SachinDevB <151764882+SachinDevB@users.noreply.github.com>
2024-01-25 20:05:10 +0530

    Create division.py

---
+++ 24-01-24/division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    result_integer = a // b
+    print(result_integer)
+ 
+    result_float = a / b
+    print(result_float)
