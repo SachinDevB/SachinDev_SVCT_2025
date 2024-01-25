92bcc6f39c03a22fd202388b2d639e45fcda3e61
SachinDevB <151764882+SachinDevB@users.noreply.github.com>
2024-01-25 19:45:01 +0530

    Create Question2.py

---
+++ 21-01-24?/Question2.py
@@ -0,0 +1,11 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+    # Integer division
+    result_integer = a // b
+    print(result_integer)
+
+    # Float division
+    result_float = a / b
+    print(result_float)
